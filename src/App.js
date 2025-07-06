3243da0cd86ba9052f770e8b42efd8baa0248e71
Saurabh Kumar Sharma <saurabhaasharma@gmail.com>
2025-07-06 11:11:50 +0530

    Update with fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js or App.jsx
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Promises from './pages/Promises';
+import Vote from './pages/Vote';
+import RaiseIssue from './pages/RaiseIssue';
+import IssueStatus from './pages/IssueStatus';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="bg-gray-100 min-h-screen">
+        <Navbar />
+        <div className="max-w-5xl mx-auto px-4 py-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/promises" element={<Promises />} />
+            <Route path="/vote" element={<Vote />} />
+            <Route path="/raise-issue" element={<RaiseIssue />} />
+            <Route path="/issue-status" element={<IssueStatus />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
